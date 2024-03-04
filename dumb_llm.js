9b4a48b39f0256f78b99afdc6f94d8b2020d7926
Dvoevodsky <dinushin@gmail.com>
2024-03-03 21:46:28 -0500

    add llm

---
+++ dumb_llm.js
@@ -0,0 +1,36 @@
+const { WebSocket } = require("ws");
+
+class DemoLlmClient {
+  constructor() {
+  }
+
+  BeginMessage(ws) {
+    const res = {
+      response_id: 0,
+      content: "How may I help you?",
+      content_complete: true,
+      end_call: false,
+    };
+    ws.send(JSON.stringify(res));
+  }
+
+  async DraftResponse(request, ws) {
+    if (request.interaction_type === "update_only") {
+      // process live transcript update if needed
+      return;
+    }
+
+    try {
+      const res = {
+        response_id: request.response_id,
+        content: "I am sorry, can you say that again?",
+        content_complete: true,
+        end_call: false,
+      };
+      ws.send(JSON.stringify(res));
+    } catch (err) {
+      console.error("Error in gpt stream: ", err);
+    }
+  }
+}
+module.exports = { DemoLlmClient };
--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+// app.listen(process.env.PORT || 3000)
+
+
+
+// npm install --save-dev @types/ws
+const WebSocket = require("ws");
+const express = require('express');
+const app = express();
+const DemoLlmClient = require("./dumb_llm").DemoLlmClient;
+const expressWs = require('express-ws')(app);
+const port = 3000;
+
+var llmClient = new DemoLlmClient();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.ws("/llm-websocket/:call_id", async (ws, req) => {
+  const callId = req.params.call_id;
+
+  ws.on("error", (err) => {
+    console.error("Error received in LLM websocket client: ", err);
+  });
+
+  // Begin message
+  llmClient.BeginMessage(ws);
+
+  ws.on("message", async (data, isBinary) => {
+    if (isBinary) {
+      console.error("Got binary message instead of text in websocket.");
+      ws.close(1002, "Cannot find corresponding Retell LLM.");
+    }
+    try {
+      const request = JSON.parse(data.toString());
+      // LLM will think about a response
+      llmClient.DraftResponse(request, ws);
+    } catch (err) {
+      console.error("Error in parsing LLM websocket message: ", err);
+      ws.close(1002, "Cannot parse incoming message.");
+    }
+  });
+});
+
+app.listen(port || process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
